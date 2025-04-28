10e3f8551a7acaac31c6630bb109feb27997dd23
vitalitygo5 <vitalitygo5@gmail.com>
2025-04-28 19:07:00 +0000

    comit tesis definitiva

---
+++ android/app/src/main/java/com/VitalityGo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.VitalityGo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.VitalityGo.app',
+  appName: 'vitalitygo',
+  webDir: 'dist'
+};
+
+export default config;
